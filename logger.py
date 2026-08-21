790498104a36ee98571a6045ea6d4e31c41d8993
AriyoAI <ariyo.a2022yo@gmail.com>
2026-08-21 17:20:50 +0330

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,18 +1,24 @@
 import logging
+import sys
 
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(message)s"
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ]
 )
 
 
 def log_info(message):
 
+    print(f"[INFO] {message}")
     logging.info(message)
 
 
 
 def log_error(message):
 
+    print(f"[ERROR] {message}")
     logging.error(message)
