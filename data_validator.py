01582a4bb28d9b806a9db76b4470b78f1706a7d3
AriyoAI <ariyo.a2022yo@gmail.com>
2026-08-21 16:40:05 +0330

    Update data_validator.py

--- data_validator.py
+++ data_validator.py
@@ -1,26 +1,17 @@
 def validate_data(data):
 
     if not data:
-
         return False
 
 
     required = [
-
         "symbol",
-
         "price",
-
         "volume_ratio",
-
         "buyer_power",
-
         "real_money",
-
         "trend",
-
         "breakout",
-
         "time"
     ]
 
@@ -30,17 +21,14 @@ def validate_data(data):
         for field in required:
 
             if field not in item:
-
                 return False
 
 
-        if not isinstance(item["symbol"], str):
-
+        if not item["symbol"]:
             return False
 
 
         if item["price"] <= 0:
-
             return False
 
 
