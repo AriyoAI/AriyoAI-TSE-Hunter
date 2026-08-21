6347556d437459d594fea8746b16d398223d8483
AriyoAI <ariyo.a2022yo@gmail.com>
2026-08-22 01:03:12 +0330

    Update report.py

--- report.py
+++ report.py
@@ -15,9 +15,7 @@ stats = {
 
 
 
-def add_scan():
-
-    stats["scans"] += 1
+def update_time():
 
     stats["last_update"] = (
         datetime.now().isoformat()
@@ -25,9 +23,19 @@ def add_scan():
 
 
 
+def add_scan():
+
+    stats["scans"] += 1
+
+    update_time()
+
+
+
 def add_signal():
 
     stats["signals"] += 1
+
+    update_time()
 
 
 
@@ -35,8 +43,20 @@ def add_error():
 
     stats["errors"] += 1
 
+    update_time()
+
 
 
 def get_report():
 
-    return stats
+    return {
+
+        "scans": stats["scans"],
+
+        "signals": stats["signals"],
+
+        "errors": stats["errors"],
+
+        "last_update": stats["last_update"]
+
+    }
